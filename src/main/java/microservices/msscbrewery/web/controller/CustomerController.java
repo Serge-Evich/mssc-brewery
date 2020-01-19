cc827bdde6e32e9715885a16b8d743da8e6cc2d2
SergeEvich <frenkdrebin@ukr.net>
2020-01-19 22:48:21 +0200

    Added validation and controllerAdvice.

--- src/main/java/microservices/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/microservices/msscbrewery/web/controller/CustomerController.java
@@ -5,15 +5,9 @@ import microservices.msscbrewery.web.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.UUID;
 
@@ -36,12 +30,12 @@ public class CustomerController {
     }
 
     @PostMapping()
-    public ResponseEntity<?> post(@RequestBody CustomerDto customerDto) {
+    public ResponseEntity<?> post(@Valid @RequestBody CustomerDto customerDto) {
         return ResponseEntity.created(URI.create(PATH + "/" + customerService.save(customerDto).getId())).build();
     }
 
     @PutMapping("/{customerId}")
-    public ResponseEntity<?> put(@PathVariable UUID customerId, @RequestBody CustomerDto customerDto) {
+    public ResponseEntity<?> put(@PathVariable UUID customerId, @Valid @RequestBody CustomerDto customerDto) {
         customerService.update(customerId, customerDto);
         return ResponseEntity.noContent().build();
     }
