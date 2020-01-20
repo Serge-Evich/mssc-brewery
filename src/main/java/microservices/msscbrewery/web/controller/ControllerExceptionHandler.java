d2e8e0549f4e5530feaddc1a4746fac84e431f72
dmytro.oleinichenko <dmytro.oleinichenko@pragmaticplay.com>
2020-01-20 13:46:40 +0200

    Fixed validation error handling.

--- src/main/java/microservices/msscbrewery/web/controller/ControllerExceptionHandler.java
+++ src/main/java/microservices/msscbrewery/web/controller/ControllerExceptionHandler.java
@@ -1,20 +1,15 @@
 package microservices.msscbrewery.web.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import javax.validation.ConstraintViolationException;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ControllerExceptionHandler {
 
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<List<String>> handle(ConstraintViolationException e) {
-        return ResponseEntity.badRequest().body(e.getConstraintViolations().stream()
-                .map(violation -> violation.getPropertyPath() + ":" + violation.getMessage())
-                .collect(Collectors.toList()));
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handle(MethodArgumentNotValidException e) {
+        return ResponseEntity.badRequest().body(e.getBindingResult().getFieldError().toString());
     }
 }
